db3a858ab8a70d526a00c30ea1307ed10ec5f345
Gene Boggs <gene@ology.net>
2024-02-29 22:47:59 -0800

    Bump version

--- lib/Music/Duration/Partition.pm
+++ lib/Music/Duration/Partition.pm
@@ -2,7 +2,7 @@ package Music::Duration::Partition;
 
 # ABSTRACT: Partition a musical duration into rhythmic phrases
 
-our $VERSION = '0.0818';
+our $VERSION = '0.0819';
 
 use Moo;
 use strictures 2;
